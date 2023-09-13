600858b7507338faf8e41b8a47de5f4bd8182b4c
Peyton-McKee <mckee.p@northeastern.edu>
2023-09-13 12:11:17 -0400

    #19 Prettier

--- scylla-server/src/index.ts
+++ scylla-server/src/index.ts
@@ -1,22 +1,22 @@
-import express, { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
+import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const port = 8000;
 const prisma = new PrismaClient();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, Express server with TypeScript!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, Express server with TypeScript!');
 });
 
-app.get("/runs", async (req: Request, res: Response) => {
+app.get('/runs', async (req: Request, res: Response) => {
   try {
     const data = await prisma.run.findUnique({
-      where: { id: 1 }, // Modify the query as needed
+      where: { id: 1 } // Modify the query as needed
     });
     res.json(data);
   } catch (error) {
-    res.status(500).json({ error: "Database error" });
+    res.status(500).json({ error: 'Database error' });
   }
 });
 
