eb3799ca47be42599d7f0b4d5cb61ce584342acf
leoleader <leoleaderlol@gmail.com>
2023-09-14 14:08:20 -0400

    updating from http to websocket

---
+++ scylla-server/src/services/systems.services.ts
@@ -0,0 +1,16 @@
+import prisma from "../prisma/prisma-client"
+import { ResponseFunction } from "../utils/message-maps.utils"
+
+// CRUD operation to get all systems with ResponseFunction type
+
+export const getAllSystems:ResponseFunction = async (data: JSON) => {
+    try {
+        const data = await prisma.system.findMany()
+        // was getting type errors trying to return just data
+        // so added JSON parse and stringify to make it conform to JSON
+        return JSON.parse(JSON.stringify(data));
+    }
+    catch (error) {
+        throw new Error("Error getting all systems")
+    }
+}
--- scylla-server/src/utils/message-maps.utils.ts
+++ scylla-server/src/utils/message-maps.utils.ts
@@ -1,4 +1,8 @@
-export type ResponseFunction = (data: JSON) => JSON;
+import { getAllSystems } from "../services/systems.services";
+
+// since these functions are async i think should output Promise<JSON> but could be wrong
+export type ResponseFunction = (data: JSON) => Promise<JSON>;
+
 /**
  * Creates a map of server messages to functions that handle the messages
  * @returns A map of server messages to functions that handle the messages
@@ -7,7 +11,7 @@ export const createServerMessageMap = (): Map<string, ResponseFunction> => {
   const serverMessageMap = new Map<string, ResponseFunction>();
   serverMessageMap.set('test', (data: JSON) => {
     console.log('test', data);
-    return data;
+    return Promise.resolve(data);
   });
   return serverMessageMap;
 };
@@ -20,7 +24,10 @@ export const createClientMessageMap = (): Map<string, ResponseFunction> => {
   const clientMessageMap = new Map<string, ResponseFunction>();
   clientMessageMap.set('test', (data: JSON) => {
     console.log('test', data);
-    return data;
+    return Promise.resolve(data);
   });
+  // adds the getAllSystems CRUD to ClientMessageMap
+  clientMessageMap.set('getAllSystems', getAllSystems)
+  console.log('getAllSystems', getAllSystems)
   return clientMessageMap;
 };
