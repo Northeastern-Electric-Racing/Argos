45d4809a138fc66bf84bd5adf718b0c941ce10b1
Peyton-McKee <mckee.p@northeastern.edu>
2023-09-14 10:05:08 -0400

    #17 Configure for Server Messages

--- scylla-server/src/proxy/proxy-controller.ts
+++ scylla-server/src/proxy/proxy-controller.ts
@@ -1,4 +1,5 @@
 import { Socket } from 'socket.io';
+import { ClientMessage, ServerMessage } from '../utils/message.utils';
 
 /**
  * Proxy Controller To Handle Inputting and Outputting Messages to a Client
@@ -6,27 +7,30 @@ import { Socket } from 'socket.io';
 export default class ProxyController {
   messageMap: Map<string, (data: JSON) => void>;
   socket: Socket;
+  server?: boolean;
 
   /**
    * Constructor
    * @param messageMap A map of input arguments to functions that handle the input
    * @param socket the socket to send and receive messages from
+   * @param server whether or not this is a server socket
    */
-  constructor(messageMap: Map<string, (data: JSON) => void>, socket: Socket) {
+  constructor(messageMap: Map<string, (data: JSON) => void>, socket: Socket, server?: boolean) {
     this.messageMap = messageMap;
     this.socket = socket;
+    this.server = server;
   }
 
   /**
-   * Handles a message from the client
+   * Handles a message received from the client
    * @param data The data received from the client
    */
   private handleClientMessage = (data: any): void => {
     try {
-      data = JSON.parse(data);
+      const decodedMessage: ClientMessage = JSON.parse(data) as ClientMessage;
       const responseFunction = this.messageMap.get(data.argument);
       if (responseFunction) {
-        const responseData = responseFunction(data);
+        const responseData = responseFunction(decodedMessage.data);
         this.socket.emit('message', responseData);
       } else {
         throw new Error('Invalid Argument');
@@ -40,25 +44,41 @@ export default class ProxyController {
   };
 
   /**
-   * Handles a client disconnect
+   * Handles a message received from the server
+   * @param data The data received from the server
    */
-  private handleClientDisconnect = (): void => {
-    console.log('disconnected from client');
+  private handleServerMessage = (data: any) => {
+    try {
+      const decodedMessage: ServerMessage = JSON.parse(data) as ServerMessage;
+      console.log(decodedMessage);
+    } catch (error) {
+      if (error instanceof Error) {
+        this.socket.emit('Error', error.message);
+      }
+      console.log('error in message', error);
+    }
   };
 
   /**
-   * Handles a client connection
+   * Handles a disconnection
    */
-  private handleClientConnection = (): void => {
-    console.log('connected to client');
-    this.socket.on('message', this.handleClientMessage);
-    this.socket.on('disconnect', this.handleClientDisconnect);
+  private handleDisconnect = (): void => {
+    console.log('disconnected socket');
+  };
+
+  /**
+   * Handles a connection
+   */
+  private handleConnection = (): void => {
+    console.log('connected socket');
+    this.socket.on('message', this.server ? this.handleServerMessage : this.handleClientMessage);
+    this.socket.on('disconnect', this.handleDisconnect);
   };
 
   /**
    * Configures the proxy controller
    */
   public configure = (): void => {
-    this.handleClientConnection();
+    this.handleConnection();
   };
 }
--- scylla-server/src/utils/message.utils.ts
+++ scylla-server/src/utils/message.utils.ts
@@ -1,11 +1,16 @@
-export type Message = {
+export type ServerMessage = {
   unix_time: number;
   node: string;
-  data: Data[];
+  data: ServerData[];
 };
 
-type Data = {
+type ServerData = {
   name: string;
   value: number;
   units: string;
 };
+
+export type ClientMessage = {
+  argument: string;
+  data: JSON;
+}
