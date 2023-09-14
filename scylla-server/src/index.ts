2f5d562451b5c92735f4680fb2705523e52859b4
Peyton-McKee <mckee.p@northeastern.edu>
2023-09-14 09:53:31 -0400

    #17 Delete Http Call

--- scylla-server/src/index.ts
+++ scylla-server/src/index.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from 'express';
 import { Server, Socket } from 'socket.io';
 import ProxyController from './proxy/proxy-controller';
-import prisma from './prisma/prisma-client';
 import { createServerMessageMap } from './utils/message-maps.utils';
 
 const app = express();
@@ -9,17 +8,6 @@ const port = 8000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, Express server with TypeScript!');
-});
-
-app.get('/runs', async (req: Request, res: Response) => {
-  try {
-    const data = await prisma.run.findUnique({
-      where: { id: 1 } // Modify the query as needed
-    });
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: 'Database error' });
-  }
 });
 
 const server = app.listen(port, () => {
