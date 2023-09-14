aaffb7322c1f17248c65bcba6943467e4f28d2b2
Peyton-McKee <mckee.p@northeastern.edu>
2023-09-14 10:12:48 -0400

    #17 Actually i think theyll be formatted the same

--- scylla-server/src/proxy/proxy-controller.ts
+++ scylla-server/src/proxy/proxy-controller.ts
@@ -1,5 +1,5 @@
 import { Socket } from 'socket.io';
-import { ClientMessage, ServerMessage } from '../utils/message.utils';
+import { ClientMessage } from '../utils/message.utils';
 
 /**
  * Proxy Controller To Handle Inputting and Outputting Messages to a Client
@@ -7,25 +7,22 @@ import { ClientMessage, ServerMessage } from '../utils/message.utils';
 export default class ProxyController {
   messageMap: Map<string, (data: JSON) => void>;
   socket: Socket;
-  server?: boolean;
 
   /**
    * Constructor
    * @param messageMap A map of input arguments to functions that handle the input
    * @param socket the socket to send and receive messages from
-   * @param server whether or not this is a server socket
    */
-  constructor(messageMap: Map<string, (data: JSON) => void>, socket: Socket, server?: boolean) {
+  constructor(messageMap: Map<string, (data: JSON) => void>, socket: Socket) {
     this.messageMap = messageMap;
     this.socket = socket;
-    this.server = server;
   }
 
   /**
-   * Handles a message received from the client
-   * @param data The data received from the client
+   * Handles a received message
+   * @param data The data received
    */
-  private handleClientMessage = (data: any): void => {
+  private handleMessage = (data: any): void => {
     try {
       const decodedMessage: ClientMessage = JSON.parse(data) as ClientMessage;
       const responseFunction = this.messageMap.get(data.argument);
@@ -35,22 +32,6 @@ export default class ProxyController {
       } else {
         throw new Error('Invalid Argument');
       }
-    } catch (error) {
-      if (error instanceof Error) {
-        this.socket.emit('Error', error.message);
-      }
-      console.log('error in message', error);
-    }
-  };
-
-  /**
-   * Handles a message received from the server
-   * @param data The data received from the server
-   */
-  private handleServerMessage = (data: any) => {
-    try {
-      const decodedMessage: ServerMessage = JSON.parse(data) as ServerMessage;
-      console.log(decodedMessage);
     } catch (error) {
       if (error instanceof Error) {
         this.socket.emit('Error', error.message);
@@ -71,7 +52,7 @@ export default class ProxyController {
    */
   private handleConnection = (): void => {
     console.log('connected socket');
-    this.socket.on('message', this.server ? this.handleServerMessage : this.handleClientMessage);
+    this.socket.on('message', this.handleMessage);
     this.socket.on('disconnect', this.handleDisconnect);
   };
 
