bb6d1c4c6a07d36d1469a6b174c969d7a03c751f
RadiantBunny633 <waasifmahmood2004@gmail.com>
2023-09-17 22:10:29 -0400

    #9

--- scylla-server/tests/dataTypes-services.test.ts
+++ scylla-server/tests/dataTypes-services.test.ts
@@ -1,5 +1,6 @@
 import prisma from '../src/prisma/prisma-client';
 import { describe, test, expect } from 'vitest';
+import { getAllDataTypes } from '../src/services/dataTypes.services';
 
 /**
  * Tests for CRUD service functions
@@ -12,7 +13,16 @@ import { describe, test, expect } from 'vitest';
 
 describe('Data Type', () => {
   test('Get All Data Types Works', async () => {
-    const allDataTypes = await prisma.dataType.findMany();
-    expect(allDataTypes).toStrictEqual([]);
+    const expected = [
+      { id: 1, dataType: 'string' },
+      { id: 2, dataType: 'int' }
+    ];
+    const prismaMock = require('../prisma/prisma-client');
+    prismaMock.dataType.findMany.mockResolvedValue(expected);
+
+    const result = await getAllDataTypes;
+
+    expect(prismaMock.dataType.findMany).toHaveBeenCalled();
+    expect(result).toBe(JSON.stringify(expected));
   });
 });
