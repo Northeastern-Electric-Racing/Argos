649ae1dd92d10e2e5a7d3d2f71faa4099b075744
RadiantBunny633 <waasifmahmood2004@gmail.com>
2023-09-17 22:25:31 -0400

    #9

--- scylla-server/tests/dataTypes-services.test.ts
+++ scylla-server/tests/dataTypes-services.test.ts
@@ -17,7 +17,7 @@ describe('Data Type', () => {
       { id: 1, dataType: 'string' },
       { id: 2, dataType: 'int' }
     ];
-    const prismaMock = require('/home/runner/work/Argos/Argos/scylla-server/tests/dataTypes-services.test.ts');
+    const prismaMock = require('/Users/waasifmahmood/desktop/argos/scylla-server/tests/dataTypes-services.test');
     prismaMock.dataType.findMany.mockResolvedValue(expected);
 
     const result = await getAllDataTypes;
