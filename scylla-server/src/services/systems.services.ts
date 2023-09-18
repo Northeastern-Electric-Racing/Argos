f2aa6b26f59af6f56e4c63646e97bf28a871c6f3
leoleader <leoleaderlol@gmail.com>
2023-09-18 19:53:13 -0400

    #23 Upsert System

--- scylla-server/src/services/systems.services.ts
+++ scylla-server/src/services/systems.services.ts
@@ -1,11 +1,29 @@
 import prisma from '../prisma/prisma-client';
-import { ResponseFunction } from '../utils/message-maps.utils';
 
 /**
  * CRUD operation to get all systems with ResponseFunction type
- * @returns string contianing all the systems in the db
+ * @returns Promise<string> contianing all the systems in the db
  */
-export const getAllSystems: ResponseFunction = async () => {
+export async function getAllSystems(): Promise<string> {
   const data = await prisma.system.findMany();
   return JSON.stringify(data);
-};
+}
+
+/**
+ * CRUD opertation that creates system if it doesn't exist, otherwise does nothing.
+ * Currently designated private so not hooked up to server.
+ * @param system_name name of the system as string
+ * @returns Promise<void>
+ */
+export async function upsertSystems(system_name: string): Promise<void> {
+  await prisma.system.upsert({
+    where: {
+      name: system_name
+    },
+    update: {},
+    create: {
+      name: system_name,
+      runs: undefined,
+    }
+  });
+}
