018fde8569b168dfa5898aa62c87302536f742de
anushkan-wani <anushkan.wani@gmail.com>
2023-09-27 17:36:29 -0400

    #22-upsert-nodes

--- scylla-server/tests/nodes-services.test.ts
+++ scylla-server/tests/nodes-services.test.ts
@@ -1,7 +1,32 @@
-import { describe, test, expect } from 'vitest';
-import { getAllNodes } from '../src/services/nodes.services';
+import { describe, test, expect, afterEach } from 'vitest';
+import { getAllNodes, upsertNode } from '../src/services/nodes.services';
+import prisma from '../src/prisma/prisma-client';
 
 describe('Node', () => {
+  //cleaning up
+  afterEach(async () => {
+    try {
+      await prisma.system.delete({
+        where: {
+          name: 'test'
+        }
+      });
+    } catch (err) {}
+  });
+
+  /**
+   * unit test for upsert node
+   * testing creating node if doesn't exist
+   */
+  test('Upsert Node Creates', async () => {
+    const expected = [{ name: 'test' }];
+    await upsertNode('test');
+    const result = JSON.parse(await getAllNodes());
+
+    // Use toEqual to compare parsedResult with the expected array
+    expect(result).toEqual(expected);
+  });
+
   test('Get All Nodes Works', async () => {
     const expected = [];
     const result = await getAllNodes();
@@ -11,5 +36,18 @@ describe('Node', () => {
 
     // Use toEqual to compare parsedResult with the expected array
     expect(parsedResult).toEqual(expected);
+  });
+
+  /**
+   * unit test for upsert node
+   * testing does nothing if node does exist
+   */
+  test('Upsert Node Does Nothing', async () => {
+    const expected = [{ name: 'test' }];
+    await upsertNode('test');
+    const result = JSON.parse(await getAllNodes());
+
+    // Use toEqual to compare result with the expected array
+    expect(result).toEqual(expected);
   });
 });
