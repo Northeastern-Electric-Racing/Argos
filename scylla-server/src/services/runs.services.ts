31a6c9167c02036fc9b010fbf8f8f42c2cdd7a57
allexandrahu <hu.ale@northeastern.edu>
2023-10-02 17:43:03 -0400

    #8-Get-All-Runs

--- scylla-server/src/services/runs.services.ts
+++ scylla-server/src/services/runs.services.ts
@@ -1,33 +1,28 @@
 import prisma from '../prisma/prisma-client';
-import { ResponseFunction } from '../types/message.types';
-
-
 
 /**
- * CRUD operation to get all runs  
- * @returns Promise<string>  all the runs  
+ * CRUD operation to get all runs
+ * @returns Promise<string>  all the runs
  */
-export const getAllRuns: ResponseFunction = async () => {
-    const data = await prisma.run.findMany();
-    return JSON.stringify(data);
-  };
-  
-  
-  
-  /**
-   * CRUD operation to get run by id
-   * @param id id of run
-   * @returns Promise<json string of run> 
-   */
-  export const getRunById = async (id: number) => {
-    const data = await prisma.run.findUnique({
-      where: {
-        id: id
-      }
-    });
-  
-    return JSON.stringify(data);
-  };
+export const getAllRuns = async () => {
+  const data = await prisma.run.findMany();
+  return JSON.stringify(data);
+};
+
+/**
+ * CRUD operation to get run by id
+ * @param id id of run
+ * @returns Promise<json string of run>
+ */
+export const getRunById = async (id: number) => {
+  const data = await prisma.run.findUnique({
+    where: {
+      id
+    }
+  });
+
+  return JSON.stringify(data);
+};
 
 /**
  * @param id id of run
@@ -35,39 +30,36 @@ export const getAllRuns: ResponseFunction = async () => {
  * @returns Promise<void>
  */
 export const upsertRun = async (id: number, locationName: string) => {
-    await prisma.run.upsert({
-      where: {
-        id: id
-      },
-      update: {
-        locationName: locationName
-      },
-      create: {
-        id: id,
-        locationName: locationName,
-        time: new Date(),
-      }
-    });
-  };
+  await prisma.run.upsert({
+    where: {
+      id
+    },
+    update: {
+      locationName
+    },
+    create: {
+      id,
+      locationName,
+      time: new Date()
+    }
+  });
+};
 
-  export const upsertLocation = async (  name: string, latitude: number, longitude: number, radius: number ) => {
-      await prisma.location.upsert({
-        where: {
-            name: name
-        },
-        update: {
-           latitude: latitude,
-           longitude: longitude,
-           radius: radius
-        },
-        create: {
-          name: name,
-           latitude: latitude,
-           longitude: longitude,
-           radius: radius
-        }
-      });
-    };
-  
-
- 
+export const upsertLocation = async (name: string, latitude: number, longitude: number, radius: number) => {
+  await prisma.location.upsert({
+    where: {
+      name
+    },
+    update: {
+      latitude,
+      longitude,
+      radius
+    },
+    create: {
+      name,
+      latitude,
+      longitude,
+      radius
+    }
+  });
+};
