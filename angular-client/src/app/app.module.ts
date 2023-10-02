ab18d9785a8e8046b93f7d8bc989d96b698a1d12
Peyton-McKee <mckee.p@northeastern.edu>
2023-10-02 11:47:37 -0400

    #46 Prettier

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -5,15 +5,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
