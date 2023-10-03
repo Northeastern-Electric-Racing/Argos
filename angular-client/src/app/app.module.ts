6044fe53f2272861ec64a6aee4c5fe80743ee7c9
Peyton-McKee <mckee.p@northeastern.edu>
2023-10-02 20:54:52 -0400

    #46 Connect Everything

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import Welcome from 'src/components/welcome/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [Welcome],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [Welcome]
 })
 export class AppModule {}
--- scylla-server/src/services/nodes.services.ts
+++ scylla-server/src/services/nodes.services.ts
@@ -1,29 +1,33 @@
+import { Node } from '@prisma/client';
 import prisma from '../prisma/prisma-client';
-import { ResponseFunction } from '../utils/message-maps.utils';
+import { ResponseFunction } from '../utils/response-function';
 
-/**
- * CRUD operation to get all systems with ResponseFunction type
- * @returns Promise<string> contianing all the nodes in the db
- */
-export const getAllNodes: ResponseFunction = async () => {
-  const data = await prisma.node.findMany();
-  return JSON.stringify(data);
-};
+export default class NodeService {
+  /**
+   * CRUD operation to get all systems with ResponseFunction type
+   * @returns Promise<string> contianing all the nodes in the db
+   */
+  static getAllNodes: ResponseFunction<Node[]> = async () => {
+    const data = await prisma.node.findMany();
+    console.log('your mom')
+    return data;
+  };
 
-/**
- * CRUD opertation that creates node if it doesn't exist, otherwise does nothing.
- * Currently designated private so not hooked up to server.
- * @param nodeName name of the system as string
- * @returns Promise<void>
- */
-export const upsertNode = async (nodeName: string) => {
-  await prisma.node.upsert({
-    where: {
-      name: nodeName
-    },
-    update: {},
-    create: {
-      name: nodeName
-    }
-  });
-};
+  /**
+   * CRUD opertation that creates node if it doesn't exist, otherwise does nothing.
+   * Currently designated private so not hooked up to server.
+   * @param nodeName name of the system as string
+   * @returns the created node
+   */
+  static upsertNode = async (nodeName: string): Promise<Node> => {
+    return await prisma.node.upsert({
+      where: {
+        name: nodeName
+      },
+      update: {},
+      create: {
+        name: nodeName
+      }
+    });
+  };
+}
