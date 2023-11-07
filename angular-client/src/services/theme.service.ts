7b8fedd0ef364762f777c390d0c6a8c90cd76387
Peyton-McKee <mckee.p@northeastern.edu>
2023-11-07 15:40:18 -0500

    #71 Bold Text

--- angular-client/src/services/theme.service.ts
+++ angular-client/src/services/theme.service.ts
@@ -6,8 +6,9 @@ export default class Theme {
   static readonly font: string = 'font-family: "Roboto"; ';
   static readonly textColor: string = 'color: #EFEFEF; ';
   static readonly textStyle: string = this.font + this.textColor;
-  static readonly HEADER: string = this.textStyle + 'fontSize: 20px; fontWeight: bold;';
+  static readonly boldedText: string = this.textStyle + 'font-weight: bold; ';
+  static readonly HEADER: string = this.boldedText + 'fontSize: 20px;';
   static readonly SUBHEADER: string = this.textStyle + 'fontSize: 16px;';
-  static readonly XXLARGEHEADER: string = this.textStyle + 'font-size: 7.5rem; padding-bottom: 60px;';
-  static readonly LARGEHEADER: string = this.textStyle + 'font-size: xx-large;';
+  static readonly XXLARGEHEADER: string = this.boldedText + 'font-size: 7.5rem; padding-bottom: 60px;';
+  static readonly LARGEHEADER: string = this.boldedText + 'font-size: xx-large;';
 }
