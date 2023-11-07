7a62bc170bf6c0e7d4585004091c9f33bf0e92ee
Peyton-McKee <mckee.p@northeastern.edu>
2023-11-07 09:43:09 -0500

    #71 Prettier

--- angular-client/src/components/loading-page/loading-page.component.ts
+++ angular-client/src/components/loading-page/loading-page.component.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-   selector: "loading-page",
-   templateUrl: "./loading-page.component.html",
-   styleUrls: ["./loading-page.component.css"]
+  selector: 'loading-page',
+  templateUrl: './loading-page.component.html',
+  styleUrls: ['./loading-page.component.css']
 })
 export default class LoadingPage {}
