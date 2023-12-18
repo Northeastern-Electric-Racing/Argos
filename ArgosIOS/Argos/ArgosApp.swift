907d083b2b1cb269a5ffa7e546548adf2d317d42
Peyton McKee <mckee.p@northeastern.edu>
2023-12-17 22:07:23 -0500

    Add Mobile App Framework

---
+++ ArgosIOS/Argos/ArgosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArgosApp.swift
+//  Argos
+//
+//  Created by Northeastern Electric Racing on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ArgosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArgosIOS/Argos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Argos
+//
+//  Created by Northeastern Electric Racing on 12/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
