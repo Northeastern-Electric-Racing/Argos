bf72deb3c6aac066e8c28024485f0f9728c9ca39
leoleader <leoleaderlol@gmail.com>
2024-01-25 19:04:44 -0500

    #68 multi inputs, got rid of item

--- angular-client/src/components/battery/battery.component.ts
+++ angular-client/src/components/battery/battery.component.ts
@@ -1,31 +1,59 @@
 import { Component, Input } from '@angular/core';
 import Theme from 'src/services/theme.service';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'battery',
   templateUrl: 'battery.component.html',
-  styleUrls: ['battery.component.css']
+  styleUrls: ['battery.component.css'],
+  animations: [
+    // metadata array
+    trigger('toggleClick', [
+      // trigger block
+      state(
+        'true',
+        style({
+          // final CSS following animation
+          backgroundColor: 'yellow'
+        })
+      ),
+      state(
+        'false',
+        style({
+          backgroundColor: 'purple'
+        })
+      ),
+      transition('true => false', animate('1000ms linear')), // animation timing
+      transition('false => true', animate('1000ms linear'))
+    ])
+  ]
 })
 export class BatteryPercentageComponent {
-  @Input() props!: { percentage: number; height: number; width: number };
+  @Input() percentage!: number;
+  @Input() height!: number;
+  @Input() width!: number;
+  heightpx!: string;
+  widthpx!: string;
   color: string = '';
-  item!: {
-    full: boolean;
-    fillColor: string;
-    backColor: string;
-    height: string;
-    width: string;
-    batHeight: string;
-    nubHeight: string;
-    nubWidth: string;
-    roundCorner: string;
-  };
+  clicked: boolean = false;
+  full!: boolean;
+  fillColor!: string;
+  backColor: string = Theme.batteryBack;
+  batHeight!: string;
+  nubHeight!: string;
+  nubWidth!: string;
+  roundCorner!: string;
 
   // setting color and rendering
   ngOnInit() {
-    if (this.props.percentage <= 20) {
+    if (this.percentage === 100) {
+      this.full = true;
+    } else {
+      this.full = false;
+    }
+    if (this.percentage <= 20) {
       this.color = Theme.battteryLow;
-    } else if (this.props.percentage <= 50) {
+    } else if (this.percentage <= 50) {
       this.color = Theme.battteryMed;
     } else {
       this.color = Theme.battteryHigh;
@@ -34,22 +62,17 @@ export class BatteryPercentageComponent {
   }
 
   // fills battery bar based on current percentage
-  // note:
   renderBattery() {
-    const batHeight = ((100 - this.props.percentage) / 100) * this.props.height + 'px';
-    const nubHeight = this.props.height / 10 + 'px';
-    const nubWidth = this.props.width / 2 + 'px';
-    const roundCorner = this.props.width / 8 + 'px';
-    this.item = {
-      full: false,
-      fillColor: this.color,
-      backColor: Theme.batteryBack,
-      height: this.props.height + 'px',
-      width: this.props.width + 'px',
-      batHeight,
-      nubHeight,
-      nubWidth,
-      roundCorner
-    };
+    this.batHeight = ((100 - this.percentage) / 100) * this.height + 'px';
+    this.nubHeight = this.height / 10 + 'px';
+    this.nubWidth = this.width / 2 + 'px';
+    this.roundCorner = this.width / 8 + 'px';
+    this.fillColor = this.color;
+    this.heightpx = this.height + 'px';
+    this.widthpx = this.width + 'px';
+  }
+
+  toggleIsCorrect() {
+    this.clicked = !this.clicked; // change in data-bound value
   }
 }
