714ef90b018419ce865b18364fd8450ee90c3d26
leoleader <leoleaderlol@gmail.com>
2024-01-30 02:20:57 -0500

    #68 rounded corners kill me

--- angular-client/src/components/battery/battery.component.ts
+++ angular-client/src/components/battery/battery.component.ts
@@ -6,7 +6,7 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   selector: 'battery',
   templateUrl: 'battery.component.html',
   styleUrls: ['battery.component.css'],
-  // this is just a dumb animationt that changes the color of the nub, would like to make something actually
+  // TO DO: this is just a dumb animation that changes the color of the nub, would like to make something actually
   // cool or interesting in the future
   animations: [
     // metadata array
@@ -45,6 +45,8 @@ export class BatteryPercentageComponent {
   nubHeight!: string;
   nubWidth!: string;
   roundCorner!: string;
+  adjustedTopCorner!: string;
+  adjustedBotCorner!: string;
 
   // setting color and rendering
   ngOnInit() {
@@ -65,13 +67,22 @@ export class BatteryPercentageComponent {
 
   // fills battery bar based on current percentage
   renderBattery() {
-    this.batHeight = ((100 - this.percentage) / 100) * this.height + 'px';
+    this.batHeight = (this.percentage / 100) * this.height + 'px';
     this.nubHeight = this.height / 10 + 'px';
     this.nubWidth = this.width / 2 + 'px';
-    this.roundCorner = this.width / 8 + 'px';
+    const minDim = Math.min(this.width, this.height);
+    this.roundCorner = minDim * 0.05 + 'px';
     this.fillColor = this.color;
     this.heightpx = this.height + 'px';
     this.widthpx = this.width + 'px';
+    this.adjustedTopCorner = '0px';
+    this.adjustedBotCorner = this.roundCorner;
+    if (this.percentage >= 95) {
+      this.adjustedTopCorner = this.roundCorner;
+    }
+    if (this.percentage <= 5 && this.percentage > 0) {
+      this.adjustedBotCorner = (this.height * this.percentage) / 100 + 'px';
+    }
   }
 
   toggleIsCorrect() {
