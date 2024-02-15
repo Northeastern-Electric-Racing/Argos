07e87b3bce0e81ab78eae32f6c4ef8b24874adfb
bpalazzi512 <60822022+bpalazzi512@users.noreply.github.com>
2024-02-15 18:33:50 -0500

    fixing prettier

--- angular-client/src/services/theme.service.ts
+++ angular-client/src/services/theme.service.ts
@@ -11,7 +11,7 @@ export default class Theme {
   static readonly SUBHEADER: string = this.textStyle + 'fontSize: 16px;';
   static readonly XXLARGEHEADER: string = this.boldedText + 'font-size: 7.5rem; margin: 0; padding: 0;';
   static readonly LARGEHEADER: string = this.boldedText + 'font-size: xx-large;';
-  static readonly INFOTITLE: string = this.textStyle + 'fontSize: 20px; font-weight: normal; margin: 0;'
+  static readonly INFOTITLE: string = this.textStyle + 'fontSize: 20px; font-weight: normal; margin: 0;';
   static readonly INFOSUBTITLE: string = this.font + 'fontSize: 16px; color: #cacaca; margin: 0;';
   static readonly battteryLow: string = '#f50905';
   static readonly battteryMed: string = '#FFEA00';
