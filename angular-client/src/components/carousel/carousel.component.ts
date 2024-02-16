aede37019b114f7c8ed27102c66ca5ece9cc5db1
Peyton-McKee <mckee.p@northeastern.edu>
2024-02-15 19:41:03 -0500

    #83 Revert even more

--- angular-client/src/components/carousel/carousel.component.ts
+++ angular-client/src/components/carousel/carousel.component.ts
@@ -51,9 +51,9 @@ export class Carousel {
   }
 
   datePipe = (date: Date) => {
-    date = new Date(date)
+    date = new Date(date);
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()} - ${date.getHours()}:${date.getMinutes()}`;
-  }
+  };
 
   selectRun = (run: Run, index: number) => {
     this.router.navigate([`graph/false/${run.id}`]);
