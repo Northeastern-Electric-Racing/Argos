b0a63c9d191cec5ceb14c71bd57eb0691e9310ec
Peyton-McKee <mckee.p@northeastern.edu>
2024-02-20 20:52:36 -0500

    #102, #88 Prettier

--- angular-client/src/utils/enumerations/style-variant.ts
+++ angular-client/src/utils/enumerations/style-variant.ts
@@ -1,4 +1,11 @@
 /**
  * Variants to be used for styling typography. Add variants for each new style.
  */
-export type StyleVariant = 'header' | 'subheader' | 'xx-large-title' | 'large-header' | 'info-title' | 'info-subtitle' | 'value';
+export type StyleVariant =
+  | 'header'
+  | 'subheader'
+  | 'xx-large-title'
+  | 'large-header'
+  | 'info-title'
+  | 'info-subtitle'
+  | 'value';
