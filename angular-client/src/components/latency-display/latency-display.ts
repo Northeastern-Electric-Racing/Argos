31d8301af8cfac961d379ef1db7748d36a840dfa
MWalve <mihirw@hotmail.com>
2024-02-25 23:49:29 -0500

    #109 Incorporated info background component

--- angular-client/src/components/latency-display/latency-display.ts
+++ angular-client/src/components/latency-display/latency-display.ts
@@ -6,20 +6,20 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./latency-display.css']
 })
 export default class LatencyDisplay {
-    @Input() latency: number = 0;
-    @Input() lowVal: number = 0;
-    @Input() medVal: number = 50;
-    @Input() highVal: number = 100;
-    @Input() height: number = 130;
-    @Input() width: number = 130;
-  
-    mapColor = (currVal: number, lowVal: number, medVal: number, highVal: number): string => {
-        if (currVal < medVal - (medVal / 2)) {
-            return '#53e400';
-          }
-          if (currVal > medVal + (medVal / 2)) {
-            return 'red';
-          }
-          return 'yellow';
-        };
+  @Input() latency: number = 0;
+  @Input() lowVal: number = 0;
+  @Input() medVal: number = 50;
+  @Input() highVal: number = 100;
+  @Input() height: number = 100;
+  @Input() width: number = 100;
+
+  mapColor = (currVal: number, lowVal: number, medVal: number, highVal: number): string => {
+    if (currVal < medVal - medVal / 2) {
+      return '#53e400';
+    }
+    if (currVal > medVal + medVal / 2) {
+      return 'red';
+    }
+    return 'yellow';
+  };
 }
