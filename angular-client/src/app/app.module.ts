8d93bd54113c614928a88cac128a7d180f3d32ea
MWalve <mihirw@hotmail.com>
2024-03-03 22:19:20 -0500

    #113 Added a divider component, few fixes

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -46,6 +46,7 @@ import { BatteryInfoDisplay } from 'src/components/battery-info-display/battery-
 import { GraphComponent } from 'src/components/graph/graph.component';
 import { InfoGraph } from 'src/components/info-graph/info-graph.component';
 import { GraphDialog } from 'src/components/graph-dialog/graph-dialog.component';
+import { Divider } from 'src/components/divider/divider';
 
 @NgModule({
   declarations: [
@@ -81,7 +82,8 @@ import { GraphDialog } from 'src/components/graph-dialog/graph-dialog.component'
     BatteryInfoDisplay,
     GraphComponent,
     InfoGraph,
-    GraphDialog
+    GraphDialog,
+    Divider,
   ],
   imports: [
     BrowserModule,
---
+++ angular-client/src/components/divider/divider.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "divider",
+  templateUrl: "./divider.html",
+  styleUrls: ["./divider.css"]
+})
+
+export class Divider{};
