308ae3b13c0015188fd0311ef72f3c87d93bde60
MWalve <mihirw@hotmail.com>
2024-03-03 22:21:59 -0500

    #113 Fixed prettier

--- angular-client/src/components/divider/divider.ts
+++ angular-client/src/components/divider/divider.ts
@@ -1,9 +1,8 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "divider",
-  templateUrl: "./divider.html",
-  styleUrls: ["./divider.css"]
+  selector: 'divider',
+  templateUrl: './divider.html',
+  styleUrls: ['./divider.css']
 })
-
-export class Divider{};
+export class Divider {}
