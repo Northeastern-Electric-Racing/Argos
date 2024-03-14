8768315a8b9b6e1fbecb87d5f0e7b1d399b4314a
MWalve <mihirw@hotmail.com>
2024-03-14 18:59:39 -0400

    #113 Finally Added Divider

--- angular-client/src/components/divider/divider.ts
+++ angular-client/src/components/divider/divider.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'divider',
   templateUrl: './divider.html',
   styleUrls: ['./divider.css']
 })
-export class Divider {}
+export class Divider {
+  @Input() height: number = 0;
+}
