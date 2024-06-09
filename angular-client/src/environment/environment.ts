d8110a1355b383b04c5b18a4b5b630b152aaf30e
RChandler234 <29521172+RChandler234@users.noreply.github.com>
2024-06-09 16:52:39 -0400

    revert changes for local dev (we gotta make this easier to do later

--- angular-client/src/environment/environment.ts
+++ angular-client/src/environment/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  production: false,
+  production: true,
+  url: 'http://192.168.100.1:8000',
   mapbox: {
     accessToken: 'pk.eyJ1IjoibWNrZWVwIiwiYSI6ImNscXBrcmU1ZTBscWIya284cDFyYjR3Nm8ifQ.6TQHlxhAJzptZyV-W28dnw'
   }
