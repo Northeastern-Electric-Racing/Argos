04cc67b3c81b2686bb418b8c0528b1d18b3608ab
Jack Rubacha <rubacha.jack03@gmail.com>
2024-07-07 17:44:55 -0400

    add a script for integration testing

---
+++ scylla-server-rust/integration_test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Starting db"
+cd ..
+docker compose run -Pd odyssey-timescale
+
+echo "Deploying prisma"
+cd ./scylla-server-rust
+cargo prisma migrate deploy
+
+echo "Running tests"
+SOURCE_DATABASE_URL=postgresql://postgres:password@127.0.0.1:5432/timescaledb cargo test -- --test-threads=1
+
+echo "Please stop your db in docker"
