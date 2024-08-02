5659a6ed61a0672fedf6816afc4d10f375e9564e
wyattb <bracy.w@northeastern.edu>
2024-08-02 15:16:23 -0400

    #161 - fault comp reset button delete oldest fault

--- angular-client/src/pages/charging-page/components/fault-display/fault-display.component.ts
+++ angular-client/src/pages/charging-page/components/fault-display/fault-display.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import Storage from 'src/services/storage.service';
 import { IdentifierDataType } from 'src/utils/enumerations/identifier-data-type';
-import { floatPipe } from 'src/utils/pipes.utils';
 
 @Component({
   selector: 'fault-display',
@@ -12,7 +11,7 @@ export default class FaultDisplay {
   faults: { faultName: string; time: string }[] = [];
   resetButton = {
     onClick: () => {
-      this.faults = [];
+      this.faults.shift();
     },
     icon: 'restart_alt'
   };
@@ -20,29 +19,33 @@ export default class FaultDisplay {
 
   ngOnInit() {
     this.storage.get(IdentifierDataType.COMM_TIMEOUT_FAULT).subscribe((value) => {
-      this.faultPushCheck(floatPipe(value.values[0]));
+      this.faultPushCheck(value.values[0], 'Comm Timeout');
     });
 
     this.storage.get(IdentifierDataType.HARDWARE_FAILURE_FAULT).subscribe((value) => {
-      this.faultPushCheck(floatPipe(value.values[0]));
+      this.faultPushCheck(value.values[0], 'Hardware Failure');
     });
 
     this.storage.get(IdentifierDataType.OVER_TEMP_FAULT).subscribe((value) => {
-      this.faultPushCheck(floatPipe(value.values[0]));
+      this.faultPushCheck(value.values[0], 'Over Temp');
     });
 
     this.storage.get(IdentifierDataType.VOLTAGE_WRONG_FAULT).subscribe((value) => {
-      this.faultPushCheck(floatPipe(value.values[0]));
+      this.faultPushCheck(value.values[0], 'Voltage Wrong');
     });
 
     this.storage.get(IdentifierDataType.WRONG_BAT_CONNECT_FAULT).subscribe((value) => {
-      this.faultPushCheck(floatPipe(value.values[0]));
+      this.faultPushCheck(value.values[0], 'Wrong Battery Connect');
     });
   }
 
-  faultPushCheck(numberValue: number) {
-    if (numberValue !== 0 && this.faults.length <= 50) {
-      this.faults.push({ faultName: 'Wrong Battery Connect', time: new Date().toLocaleTimeString() });
+  // Takes in a string (which should be an integer) representing the storage value of a fault, and the faults name.
+  // If the string is 0 their is no fault, anything else means there was a fault.
+  // (this is check is required because the storage service still takes information from the server even if a fault hasn't happened, requiring
+  // us to ensure that change to the storage value was actually a fault)
+  faultPushCheck(addFault: string, faultName: string) {
+    if (parseInt(addFault) !== 0) {
+      this.faults.push({ faultName, time: new Date().toLocaleTimeString() });
     }
   }
 }
