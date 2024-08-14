c7465222f42f82c7b6b4642e1ef25400c8e86980
RChandler234 <29521172+RChandler234@users.noreply.github.com>
2024-08-13 21:12:48 -0400

    #178 - unused import

--- angular-client/src/pages/graph-page/graph/graph.component.ts
+++ angular-client/src/pages/graph-page/graph/graph.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import * as ApexCharts from 'apexcharts';
 import { ApexXAxis, ApexDataLabels, ApexChart, ApexMarkers, ApexGrid, ApexTooltip, ApexFill } from 'ng-apexcharts';
 import { BehaviorSubject } from 'rxjs';
