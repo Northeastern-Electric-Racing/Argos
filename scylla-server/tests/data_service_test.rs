ac905cc3a9c6f72d86ba39db8b3f8fef3ad13e2d
Peyton-McKee <mckee.p@northeastern.edu>
2025-02-09 11:56:17 -0500

    #275 prettier

--- scylla-server/tests/data_service_test.rs
+++ scylla-server/tests/data_service_test.rs
@@ -23,12 +23,8 @@ async fn test_data_service() -> Result<(), diesel::result::Error> {
     )
     .await?;
     // node_service::upsert_node(&db, TEST_KEYWORD.to_owned()).await?;
-    data_type_service::upsert_data_type(
-        &mut db,
-        TEST_KEYWORD.to_owned(),
-        "joe_mama".to_owned(),
-    )
-    .await?;
+    data_type_service::upsert_data_type(&mut db, TEST_KEYWORD.to_owned(), "joe_mama".to_owned())
+        .await?;
     data_service::get_data(&mut db, TEST_KEYWORD.to_owned(), 0).await?;
 
     Ok(())
@@ -40,12 +36,8 @@ async fn test_data_add() -> Result<(), diesel::result::Error> {
     let mut db = pool.get().await.unwrap();
 
     // node_service::upsert_node(&db, TEST_KEYWORD.to_owned()).await?;
-    data_type_service::upsert_data_type(
-        &mut db,
-        TEST_KEYWORD.to_owned(),
-        "joe mama".to_owned(),
-    )
-    .await?;
+    data_type_service::upsert_data_type(&mut db, TEST_KEYWORD.to_owned(), "joe mama".to_owned())
+        .await?;
     let run_data = run_service::create_run(
         &mut db,
         chrono::DateTime::from_timestamp_millis(999).unwrap(),
@@ -109,12 +101,8 @@ async fn test_data_no_prereqs() -> Result<(), diesel::result::Error> {
 
     // now add the node, datatype, and run
     // node_service::upsert_node(&db, TEST_KEYWORD.to_owned()).await?;
-    data_type_service::upsert_data_type(
-        &mut db,
-        TEST_KEYWORD.to_owned(),
-        "ur mom".to_owned(),
-    )
-    .await?;
+    data_type_service::upsert_data_type(&mut db, TEST_KEYWORD.to_owned(), "ur mom".to_owned())
+        .await?;
     run_service::create_run_with_id(
         &mut db,
         chrono::DateTime::from_timestamp_millis(1000).unwrap(),
