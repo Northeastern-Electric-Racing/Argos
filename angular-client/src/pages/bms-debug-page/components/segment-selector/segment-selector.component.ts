bcba8755f7acfe8bddd26f53f9d3e78cf0fe64c7
wyattb <bracy.w@northeastern.edu>
2025-02-24 02:27:47 -0500

    starting layout for the accumulator summary page

---
+++ angular-client/src/pages/bms-debug-page/components/segment-selector/segment-selector.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'segment-selector',
+  templateUrl: './segment-selector.component.html',
+  styleUrl: './segment-selector.component.css'
+})
+export class SegmentSelectorComponent {}
---
+++ angular-client/src/pages/bms-debug-page/components/segment-summary/segment-summary.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'segment-summary',
+  templateUrl: './segment-summary.component.html',
+  styleUrl: './segment-summary.component.css'
+})
+export class SegmentSummaryComponent {}
