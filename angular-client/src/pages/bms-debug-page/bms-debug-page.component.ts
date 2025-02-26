0479f37c570aa82cadbcc7332371653b29f8fd39
wyattb <bracy.w@northeastern.edu>
2025-02-26 01:08:34 -0500

    Added temps to seg sum, added ui small width support

--- angular-client/src/pages/bms-debug-page/bms-debug-page.component.ts
+++ angular-client/src/pages/bms-debug-page/bms-debug-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import { SegmentSummarys } from './components/segment-summary/segment-summary.component';
 
 @Component({
   selector: 'app-bms-debug-page',
@@ -9,7 +10,15 @@ export class BmsDebugPageComponent implements OnInit {
   time = new Date();
   newRunIsLoading = false;
   mobileThreshold = 768;
+  windowSize: number = window.innerWidth;
   isMobile = window.innerWidth < this.mobileThreshold;
+  segments = [
+    SegmentSummarys.Segment1,
+    SegmentSummarys.Segment2,
+    SegmentSummarys.Segment3,
+    SegmentSummarys.Segment4,
+    SegmentSummarys.Segment5
+  ];
 
   constructor() {}
 
@@ -20,5 +29,6 @@ export class BmsDebugPageComponent implements OnInit {
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.isMobile = window.innerWidth <= this.mobileThreshold;
+    this.windowSize = window.innerWidth;
   }
 }
