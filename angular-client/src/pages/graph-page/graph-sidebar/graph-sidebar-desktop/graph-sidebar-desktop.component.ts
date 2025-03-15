bd8f4e2693a1d802f6ee720aa52630d69190bec1
Peyton-McKee <mckee.p@northeastern.edu>
2025-03-15 14:13:32 -0400

    Add Data Labels In Sidebar

--- angular-client/src/pages/graph-page/graph-sidebar/graph-sidebar-desktop/graph-sidebar-desktop.component.ts
+++ angular-client/src/pages/graph-page/graph-sidebar/graph-sidebar-desktop/graph-sidebar-desktop.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit, OnDestroy, input } from '@angular/core';
-import { DataType, Node, NodeWithVisibilityToggle } from 'src/utils/types.utils';
+import { Component, OnInit, OnDestroy, input, inject } from '@angular/core';
+import { DataType, Node } from 'src/utils/types.utils';
 import { FormControl, FormGroup } from '@angular/forms';
-import { debounceTime, Observable, of, Subscription } from 'rxjs';
+import { BehaviorSubject, debounceTime, Subscription } from 'rxjs';
 import { dataTypesToNodes } from 'src/utils/dataTypes.utils';
 import { dataTypeNamePipe } from 'src/utils/dataTypes.utils';
 import { TreeNode } from 'primeng/api';
+import Storage from 'src/services/storage.service';
+import { decimalPipe } from 'src/utils/pipes.utils';
 
 /**
  * Sidebar component that displays the nodes and their data types.
@@ -18,10 +20,10 @@ import { TreeNode } from 'primeng/api';
   styleUrls: ['./graph-sidebar-desktop.component.css']
 })
 export default class GraphSidebarDesktopComponent implements OnInit, OnDestroy {
+  private storage = inject(Storage);
   dataTypes = input<DataType[]>([]);
   selectDataType = input.required<(dataType: DataType) => void>();
   nodes: Node[] = [];
-  nodesWithVisibilityToggle!: Observable<NodeWithVisibilityToggle[]>;
 
   filterForm: FormGroup = new FormGroup({
     searchFilter: new FormControl<string>('')
@@ -39,26 +41,24 @@ export default class GraphSidebarDesktopComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.nodes = dataTypesToNodes(this.dataTypes());
 
-    this.nodesWithVisibilityToggle = of(
-      this.nodes.map((node: Node) => {
-        return {
-          ...node,
-          subnodesVisible: false
-        };
-      })
-    );
-
     // Callback to update search regex (debounced at 300 ms)
     this.filterFormSubsription = this.filterForm.valueChanges.pipe(debounceTime(300)).subscribe((changes) => {
       this.searchFilter = changes.searchFilter;
     });
 
-    const mapToTreeNode = (node: Node): TreeNode => ({
-      label: node.name,
-      data: node,
-      key: node.topicName,
-      children: node.nodes.value.map(mapToTreeNode)
-    });
+    const mapToTreeNode = (node: Node): TreeNode => {
+      const displayValue = new BehaviorSubject<string>('N/A');
+      this.storage.get(node.topicName.slice(0, -1)).subscribe((value) => {
+        console.log(value);
+        displayValue.next(decimalPipe(value.values[0], 3).toFixed(3) + value.unit);
+      });
+      return {
+        label: node.name,
+        data: { ...node, displayValue },
+        key: node.topicName,
+        children: node.nodes.value.map(mapToTreeNode)
+      };
+    };
 
     this.treeNodes = this.nodes.map(mapToTreeNode);
   }
