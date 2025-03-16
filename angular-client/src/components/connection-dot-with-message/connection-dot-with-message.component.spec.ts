93502c5add05e7da23f2d4fc5c943e1a08a03ea2
wyattb <bracy.w@northeastern.edu>
2025-03-16 17:36:10 -0400

    #325 - bms header and connection dot abstraction

---
+++ angular-client/src/components/connection-dot-with-message/connection-dot-with-message.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConnectionDotWithMessageComponent } from './connection-dot-with-message.component';
+
+describe('ConnectionDotWithMessageComponent', () => {
+  let component: ConnectionDotWithMessageComponent;
+  let fixture: ComponentFixture<ConnectionDotWithMessageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ConnectionDotWithMessageComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ConnectionDotWithMessageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
