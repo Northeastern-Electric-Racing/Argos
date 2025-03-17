440c32174347ad8db09fd191c8d64588866ad594
wyattb <bracy.w@northeastern.edu>
2025-03-17 00:20:57 -0400

    #296 - added adbms component logic

--- angular-client/src/pages/charging-page/components/high-low-cell/high-low-cell-display/high-low-cell-display.component.ts
+++ angular-client/src/pages/charging-page/components/high-low-cell/high-low-cell-display/high-low-cell-display.component.ts
@@ -32,12 +32,12 @@ export default class HighLowCellDisplayComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.storage.get(DataTypeEnum.VOLTS_LOW).subscribe((value) => {
+    this.storage.get(DataTypeEnum.CELL_VOLTS_LOW).subscribe((value) => {
       this.lowCellVoltage = decimalPipe(value.values[0], 3);
       this.delta = decimalPipe((this.highCellVoltage - this.lowCellVoltage).toFixed(3), 3);
       this.lowVoltsData.push({ x: +value.time, y: this.lowCellVoltage });
     });
-    this.storage.get(DataTypeEnum.VOLTS_HIGH).subscribe((value) => {
+    this.storage.get(DataTypeEnum.CELL_VOLTS_HIGH).subscribe((value) => {
       this.highCellVoltage = decimalPipe(value.values[0], 3);
       this.delta = decimalPipe((this.highCellVoltage - this.lowCellVoltage).toFixed(3), 3);
       this.highVoltsData.push({ x: +value.time, y: this.highCellVoltage });
