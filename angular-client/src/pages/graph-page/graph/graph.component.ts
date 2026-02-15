f688203f33a0de1b4bd0a4c44e0a5d4b8e1bd774
wyattb <bracy.w@northeastern.edu>
2026-02-15 18:28:22 -0500

    #467 - add ref for shift bit

--- angular-client/src/pages/graph-page/graph/graph.component.ts
+++ angular-client/src/pages/graph-page/graph/graph.component.ts
@@ -22,6 +22,8 @@ function binarySearchInsertIndex(arr: { x: number }[], x: number): number {
   let lo = 0;
   let hi = arr.length;
   while (lo < hi) {
+    // shift right, same as mid = Math.floor((lo + hi) / 2) but avoids potential overflow
+    // interesting article: https://research.google/blog/extra-extra-read-all-about-it-nearly-all-binary-searches-and-mergesorts-are-broken/
     const mid = (lo + hi) >>> 1;
     if (arr[mid].x < x) {
       lo = mid + 1;
