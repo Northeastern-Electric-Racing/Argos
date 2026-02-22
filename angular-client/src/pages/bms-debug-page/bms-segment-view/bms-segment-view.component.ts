b904a10961764aa1a4b6bc751f29f7fc25874d28
wyattb <bracy.w@northeastern.edu>
2026-02-22 11:24:18 -0500

    #527 Port hex heatmap to segment detail page

--- angular-client/src/pages/bms-debug-page/bms-segment-view/bms-segment-view.component.ts
+++ angular-client/src/pages/bms-debug-page/bms-segment-view/bms-segment-view.component.ts
@@ -1,13 +1,20 @@
-import { Component, HostListener, OnInit } from '@angular/core';
-import { inject } from '@angular/core';
+import { Component, HostListener, inject, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { allSegments, Chip, Segment } from 'src/utils/bms.utils';
 import { MatGridList, MatGridTile } from '@angular/material/grid-list';
 import { BmsHeaderComponent } from '../components/bms-header/bms-header.component';
 import { SegmentAtAGlanceComponent } from '../components/segment-at-a-glance/segment-at-a-glance.component';
-import { CellByCellHeatMapComponent } from '../components/cell-by-cell-heat-map/cell-by-cell-heat-map.component';
+import { SegmentHeatmapComponent } from '../components/segment-heatmap/segment-heatmap.component';
 import { ChipDiagnosticsComponent } from '../components/chip-diagnostics/chip-diagnostics.component';
 import { ChipFaultsComponent } from '../components/chip-faults/chip-faults.component';
+import { InfoBackgroundComponent } from '../../../components/info-background/info-background.component';
+import { DropdownOption, SelectorConfig } from 'src/components/select-dropdown/select-dropdown.component';
+import { HeatMapService, HeatMapView } from 'src/services/heat-map.service';
+
+const formatAllSelectorName = (name: string) => {
+  return 'Set ALL Maps: ' + name;
+};
 
 @Component({
   selector: 'bms-segment-view',
@@ -19,21 +26,82 @@ import { ChipFaultsComponent } from '../components/chip-faults/chip-faults.compo
     MatGridTile,
     BmsHeaderComponent,
     SegmentAtAGlanceComponent,
-    CellByCellHeatMapComponent,
+    SegmentHeatmapComponent,
+    InfoBackgroundComponent,
     ChipDiagnosticsComponent,
     ChipFaultsComponent
   ]
 })
-export class BmsSegmentViewComponent implements OnInit {
+export class BmsSegmentViewComponent implements OnInit, OnDestroy {
   private readonly route = inject(ActivatedRoute);
   private router = inject(Router);
+  private heatMapService = inject(HeatMapService);
+  private subscriptions: Subscription[] = [];
   changeTitleSize = window.innerWidth < 1060;
   segmentId!: Segment;
   chipAlpha: Chip = Chip.Alpha;
   chipBeta: Chip = Chip.Beta;
 
+  cellViewSelectOptions: DropdownOption[] = [
+    {
+      name: HeatMapView.Temperature.toString(),
+      function: () => {
+        this.heatMapService.setCurrentView(this.segmentId, HeatMapView.Temperature);
+      }
+    },
+    {
+      name: HeatMapView.Voltage.toString(),
+      function: () => {
+        this.heatMapService.setCurrentView(this.segmentId, HeatMapView.Voltage);
+      }
+    },
+    {
+      name: HeatMapView.Balancing.toString(),
+      function: () => {
+        this.heatMapService.setCurrentView(this.segmentId, HeatMapView.Balancing);
+      }
+    }
+  ];
+
+  currentSegmentSelectorConfig: SelectorConfig = {
+    options: this.cellViewSelectOptions,
+    placeholder: 'Change View'
+  };
+
+  allSegSelectorConfig: SelectorConfig = {
+    options: this.cellViewSelectOptions.map((option) => ({
+      name: formatAllSelectorName(option.name),
+      function: () => {
+        this.heatMapService.setAllSegViews(option.name as HeatMapView);
+      }
+    })),
+    placeholder: 'Change ALL Segments'
+  };
+
   ngOnInit(): void {
     this.subscribeToSegmentID();
+  }
+
+  getHeatmapTitle(): string {
+    return 'Segment ' + (this.segmentId + 1) + ': Cell-by-Cell';
+  }
+
+  private subscribeToView(): void {
+    const viewSub = this.heatMapService.getCurrentView(this.segmentId);
+    if (viewSub) {
+      this.subscriptions.push(
+        viewSub.subscribe((view) => {
+          this.allSegSelectorConfig = {
+            ...this.allSegSelectorConfig,
+            defaultValue: view !== undefined ? formatAllSelectorName(view.toString()) : 'Change ALL Segments'
+          };
+          this.currentSegmentSelectorConfig = {
+            ...this.currentSegmentSelectorConfig,
+            defaultValue: view !== undefined ? view : 'Change View'
+          };
+        })
+      );
+    }
   }
 
   // Update view width
@@ -47,9 +115,14 @@ export class BmsSegmentViewComponent implements OnInit {
       this.route.paramMap.subscribe((params) => {
         const possibleSegId = Number(params.get('id')) - 1;
         allSegments.indexOf(possibleSegId) !== -1 ? (this.segmentId = possibleSegId) : this.router.navigate(['bms']);
+        this.subscribeToView();
       });
     } else {
       this.router.navigate(['bms']);
     }
   };
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((s) => s.unsubscribe());
+  }
 }
