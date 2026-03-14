35a852f400dc66b809056e21b5628df1c12e29d8
bjackson312006 <blakejackson312006@gmail.com>
2026-03-13 20:21:02 -0400

    better sizing on weird screens

--- angular-client/src/pages/efuses-page/efuses-page.component.ts
+++ angular-client/src/pages/efuses-page/efuses-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { Subscription } from 'rxjs';
 import Storage from 'src/services/storage.service';
 import { DataTypeEnum } from 'src/data-type.enum';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 import { MatGridList, MatGridTile } from '@angular/material/grid-list';
 import TypographyComponent from 'src/components/typography/typography.component';
@@ -19,14 +20,40 @@ import EfuseCardComponent from './components/efuse-card/efuse-card.component';
 })
 export default class EfusesPageComponent implements OnInit, OnDestroy {
   private storage = inject(Storage);
+  private breakpointObserver = inject(BreakpointObserver);
   private subscriptions: Subscription[] = [];
 
   // Expose DataTypeEnum for template
   DataTypeEnum = DataTypeEnum;
 
+  gridCols = 3;
+  gridRowHeight: string | number = '450px';
+
+  private readonly singleColumnQuery = '(max-width: 1100px)';
+  private readonly doubleColumnQuery = '(max-width: 1800px)';
+
   ngOnInit() {
     // Page initialization
     console.log('eFuses page initialized');
+
+    this.subscriptions.push(
+      this.breakpointObserver.observe([this.singleColumnQuery, this.doubleColumnQuery]).subscribe((state) => {
+        if (state.breakpoints[this.singleColumnQuery]) {
+          this.gridCols = 1;
+          this.gridRowHeight = '520px';
+          return;
+        }
+
+        if (state.breakpoints[this.doubleColumnQuery]) {
+          this.gridCols = 2;
+          this.gridRowHeight = '500px';
+          return;
+        }
+
+        this.gridCols = 3;
+        this.gridRowHeight = '450px';
+      })
+    );
   }
 
   ngOnDestroy() {
