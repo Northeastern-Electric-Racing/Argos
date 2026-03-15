bb231c0bb2ab0a978813a0ef00e2b5cea83192ce
wyattb <bracy.w@northeastern.edu>
2026-03-15 18:23:44 -0400

    add custom grid layout

--- angular-client/src/pages/efuses-page/efuses-page.component.ts
+++ angular-client/src/pages/efuses-page/efuses-page.component.ts
@@ -1,13 +1,11 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
-import Storage from 'src/services/storage.service';
 import { DataTypeEnum } from 'src/data-type.enum';
-import { BreakpointObserver } from '@angular/cdk/layout';
 
-import { MatGridList, MatGridTile } from '@angular/material/grid-list';
 import TypographyComponent from 'src/components/typography/typography.component';
 import EfuseCardComponent from './components/efuse-card/efuse-card.component';
 import RtdsDebugCardComponent from './components/rtds-debug-card/rtds-debug-card.component';
+import GridLayoutComponent from 'src/components/grid-layout/grid-layout.component';
 
 /**
  * Container for the eFuses page, displays eFuse status and controls.
@@ -17,44 +15,17 @@ import RtdsDebugCardComponent from './components/rtds-debug-card/rtds-debug-card
   styleUrls: ['./efuses-page.component.css'],
   templateUrl: './efuses-page.component.html',
   standalone: true,
-  imports: [MatGridList, MatGridTile, TypographyComponent, EfuseCardComponent, RtdsDebugCardComponent]
+  imports: [GridLayoutComponent, TypographyComponent, EfuseCardComponent, RtdsDebugCardComponent]
 })
 export default class EfusesPageComponent implements OnInit, OnDestroy {
-  private storage = inject(Storage);
-  private breakpointObserver = inject(BreakpointObserver);
   private subscriptions: Subscription[] = [];
 
   // Expose DataTypeEnum for template
   DataTypeEnum = DataTypeEnum;
 
-  gridCols = 3;
-  gridRowHeight: string | number = '450px';
-
-  private readonly singleColumnQuery = '(max-width: 1100px)';
-  private readonly doubleColumnQuery = '(max-width: 1800px)';
-
   ngOnInit() {
     // Page initialization
     console.log('eFuses page initialized');
-
-    this.subscriptions.push(
-      this.breakpointObserver.observe([this.singleColumnQuery, this.doubleColumnQuery]).subscribe((state) => {
-        if (state.breakpoints[this.singleColumnQuery]) {
-          this.gridCols = 1;
-          this.gridRowHeight = '520px';
-          return;
-        }
-
-        if (state.breakpoints[this.doubleColumnQuery]) {
-          this.gridCols = 2;
-          this.gridRowHeight = '500px';
-          return;
-        }
-
-        this.gridCols = 3;
-        this.gridRowHeight = '450px';
-      })
-    );
   }
 
   ngOnDestroy() {
